59ac463ec9752b0129cb5cfedab222a6a1737cf3
Adpros7 <pranit.advik@gmail.com>
2026-08-25 14:18:37 -0400

    forgot to commit, but im done

---
+++ fix_stl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env/ python3
+
+
+import argparse
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import pymeshfix
+import pymeshlab
+import trimesh
+from trimesh.exchange.load import mesh_formats
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path")
+
+path = str(parser.parse_args()._get_kwargs()[0][1])
+
+mesh = trimesh.load(path)
+v, f = mesh.vertices, mesh.faces  # ty:ignore[unresolved-attribute]  # pyright: ignore[reportAttributeAccessIssue]
+
+meshfix = pymeshfix.MeshFix(v, f)
+meshfix.repair()
+
+v, f = meshfix.mesh.points, meshfix.mesh.faces.reshape(-1, 4)[:, 1:]  # pyright: ignore[reportAttributeAccessIssue]
+
+meshfix.clean()
+meshfix.save(
+    f"{"/".join(path.split("/")[:-2])}/{path.removesuffix('.stl')}_{str(datetime.now(ZoneInfo('America/New_York'))).replace(' ', '')}.stl"
+)
