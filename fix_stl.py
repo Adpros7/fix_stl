ac484a69a670626eae4f03e84c69741c9acef4c7
Adpros7 <pranit.advik@gmail.com>
2026-09-01 12:59:28 -0400

    use trimesh.Trimesh

--- fix_stl.py
+++ fix_stl.py
@@ -10,14 +10,21 @@ parser.add_argument("path")
 path = str(parser.parse_args()._get_kwargs()[0][1])
 
 mesh = trimesh.load(path)
+
 v, f = mesh.vertices, mesh.faces  # ty:ignore[unresolved-attribute]  # pyright: ignore[reportAttributeAccessIssue]
 
 meshfix = pymeshfix.MeshFix(v, f)
 meshfix.repair()
+meshfix.clean()
 
 v, f = meshfix.mesh.points, meshfix.mesh.faces.reshape(-1, 4)[:, 1:]  # pyright: ignore[reportAttributeAccessIssue]
 
-meshfix.clean()
+fixer = trimesh.Trimesh(v, f)
+fixer.fill_holes()
+fixer.fix_normals()
+
+
+
 meshfix.save(
     f"{path.removesuffix('.stl')}_fixed__{str(datetime.now(ZoneInfo('America/New_York'))).replace(' ', '')}.stl"
 )
