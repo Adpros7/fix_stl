a630b403596f17f96bdd7f8ecf7dfcd8ff856d35
Adpros7 <pranit.advik@gmail.com>
2026-09-01 13:09:03 -0400

    get args normally

--- fix_stl.py
+++ fix_stl.py
@@ -8,14 +8,14 @@ import manifold3d
 parser = argparse.ArgumentParser()
 parser.add_argument("path")
 
-path = str(parser.parse_args()._get_kwargs()[0][1])
+path = str(parser.parse_args().path)
 
-mesh = trimesh.load(path)
+mesh = trimesh.load_mesh(path)
 
-v, f = mesh.vertices, mesh.faces  # ty:ignore[unresolved-attribute]  # pyright: ignore[reportAttributeAccessIssue]
+v, f = mesh.vertices, mesh.faces  # pyright: ignore[reportAttributeAccessIssue]
 
 meshfix = pymeshfix.MeshFix(v, f)
-meshfix.repair()
+meshfix.repair(remove_smallest_components=False)
 meshfix.clean()
 
 v, f = meshfix.mesh.points, meshfix.mesh.faces.reshape(-1, 4)[:, 1:]  # pyright: ignore[reportAttributeAccessIssue]
@@ -31,4 +31,6 @@ m3d.merge()
 
 v, f = m3d.vert_properties, m3d.tri_verts
 
-trimesh.Trimesh(v, f).export(f"{path.removesuffix('.stl')}_fixed__{str(datetime.now(ZoneInfo('America/New_York'))).replace(' ', '')}.stl")
+trimesh.Trimesh(v, f).export(
+    f"{path.removesuffix('.stl')}_fixed__{str(datetime.now(ZoneInfo('America/New_York'))).replace(' ', '')}.stl"
+)
